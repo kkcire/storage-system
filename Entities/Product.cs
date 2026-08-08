02d71a0726949e8ebcdef124e5928f5e4f6632e9
kcire <erickmagagna84@gmail.com>
2026-08-08 12:43:15 -0300

    feat: add required modifier to Product.Name

--- Entities/Product.cs
+++ Entities/Product.cs
@@ -3,7 +3,7 @@ namespace StorageSystem.Entities;
 public class Product
 {
     public int Id { get; set; }
-    public string? Name { get; set; }
+    public required string Name { get; set; }
     public decimal Price { get; set; }
     public int Quantity { get; set; }
     public int BrandId { get; set; }
