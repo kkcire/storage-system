5e8885a15235ff6c09f747e85ce1dd3826cd8c47
kcire <erickmagagna84@gmail.com>
2026-08-08 19:56:42 -0300

    refactor: rename methods and reorganize class member order

--- src/StorageSystem/Services/BrandService.cs
+++ src/StorageSystem/Services/BrandService.cs
@@ -5,7 +5,7 @@ namespace StorageSystem.Services;
 
 public class BrandService(StorageContext context)
 {
-    public Brand AddBrand(string name)
+    public Brand Add(string name)
     {
         if (string.IsNullOrWhiteSpace(name))
             throw new ArgumentException("The name of the Brand cannot be empty.");
@@ -16,32 +16,7 @@ public class BrandService(StorageContext context)
         return brand;
     }
 
-    public Brand GetById(int id)
-    {
-        if (id <= 0)
-            throw new ArgumentException("The ID cannot be zero or negative.");
-
-        var brand = context.Brands.FirstOrDefault(b => b.Id == id);
-
-        if (brand == null)
-            throw new KeyNotFoundException($"The brand with ID {id} was not found.");
-
-        return brand;
-    }
-    public List<Brand> SearchByName(string name)
-    {
-        if (string.IsNullOrWhiteSpace(name))
-            throw new ArgumentException("The name of the Brand cannot be empty.");
-
-        return context.Brands.Where(b => b.Name.Contains(name)).ToList();
-    }
-
-    public List<Brand> GetAll()
-    {
-        return context.Brands.ToList();
-    }
-
-    public Brand UpdateBrand(int id, string newName)
+    public Brand Update(int id, string newName)
     {
         if (id <= 0)
             throw new ArgumentException("The ID cannot be zero or negative.");
@@ -60,7 +35,7 @@ public class BrandService(StorageContext context)
         return brand;
     }
 
-    public string DeleteBrand(int id)
+    public string Delete(int id)
     {
         if (id <= 0)
             throw new ArgumentException("The ID cannot be zero or negative.");
@@ -76,4 +51,29 @@ public class BrandService(StorageContext context)
         return brand.Name;
     }
 
+    public Brand GetById(int id)
+    {
+        if (id <= 0)
+            throw new ArgumentException("The ID cannot be zero or negative.");
+
+        var brand = context.Brands.FirstOrDefault(b => b.Id == id);
+
+        if (brand == null)
+            throw new KeyNotFoundException($"The brand with ID {id} was not found.");
+
+        return brand;
+    }
+    
+    public List<Brand> SearchByName(string name)
+    {
+        if (string.IsNullOrWhiteSpace(name))
+            throw new ArgumentException("The name of the Brand cannot be empty.");
+
+        return context.Brands.Where(b => b.Name.Contains(name)).ToList();
+    }
+
+    public List<Brand> GetAll()
+    {
+        return context.Brands.ToList();
+    }
 }
