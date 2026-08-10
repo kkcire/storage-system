8658be4a79d5af495766897a82eb2b6a3326049a
kcire <erickmagagna84@gmail.com>
2026-08-10 00:07:01 -0300

    refactor: switching the hand write guards to methods of guard clause class

--- src/StorageSystem/Services/BrandService.cs
+++ src/StorageSystem/Services/BrandService.cs
@@ -7,8 +7,7 @@ public class BrandService(StorageContext context)
 {
     public Brand Add(string name)
     {
-        if (string.IsNullOrWhiteSpace(name))
-            throw new ArgumentException("The name of the Brand cannot be empty.");
+        GuardClause.ValidateNullOrEmptyName(name);
 
         Brand brand = new() { Name = name };
         context.Brands.Add(brand);
@@ -18,16 +17,9 @@ public class BrandService(StorageContext context)
 
     public Brand Update(int id, string newName)
     {
-        if (id <= 0)
-            throw new ArgumentException("The ID cannot be zero or negative.");
+        GuardClause.ValidateNullOrEmptyName(newName);
 
-        if (string.IsNullOrWhiteSpace(newName))
-            throw new ArgumentException("The new name cannot be empty.");
-
-        var brand = context.Brands.FirstOrDefault(b => b.Id == id);
-
-        if (brand == null)
-            throw new KeyNotFoundException($"The brand with ID {id} was not found.");
+        var brand = GetById(id);
 
         brand.Name = newName;
         context.SaveChanges();
@@ -37,13 +29,7 @@ public class BrandService(StorageContext context)
 
     public string Delete(int id)
     {
-        if (id <= 0)
-            throw new ArgumentException("The ID cannot be zero or negative.");
-
-        var brand = context.Brands.FirstOrDefault(b => b.Id == id);
-
-        if (brand == null)
-            throw new KeyNotFoundException($"The brand with ID {id} was not found.");
+        var brand = GetById(id);
 
         context.Brands.Remove(brand);
         context.SaveChanges();
@@ -53,21 +39,19 @@ public class BrandService(StorageContext context)
 
     public Brand GetById(int id)
     {
-        if (id <= 0)
-            throw new ArgumentException("The ID cannot be zero or negative.");
+        GuardClause.ValidateZeroOrNegativeId(id);
 
-        var brand = context.Brands.FirstOrDefault(b => b.Id == id);
+        var brand = context.Brands.Find(id);
 
         if (brand == null)
             throw new KeyNotFoundException($"The brand with ID {id} was not found.");
 
         return brand;
     }
-    
+
     public List<Brand> SearchByName(string name)
     {
-        if (string.IsNullOrWhiteSpace(name))
-            throw new ArgumentException("The name of the Brand cannot be empty.");
+        GuardClause.ValidateNullOrEmptyName(name);
 
         return context.Brands.Where(b => b.Name.Contains(name)).ToList();
     }
